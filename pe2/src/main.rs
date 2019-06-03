781e35cd3f508f5af96acba0f1ff41c30c7e37b7
Jon Moroney <darakian@gmail.com>
2019-06-03 15:38:53 -0700

    pe2

---
+++ pe2/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut the_sum = 0;
+    let mut i = 1;
+    let mut j = 1;
+    while let current = fib_next(i, j){
+        if current%2 == 0{
+            the_sum+=current;
+        }
+        if current >= 4000000{
+            break;
+        }
+        i = j;
+        j = current;
+    }
+    println!("{}", the_sum);
+}
+
+fn fib_next(n: u32, m: u32) -> u32{
+    return n+m
+}
