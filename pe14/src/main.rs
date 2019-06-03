3bd96df992fa7c9945c12b0b87d1482425a12528
Jon Moroney <darakian@gmail.com>
2019-06-03 14:18:31 -0700

    pe14

--- pe14/src/main.rs
+++ pe14/src/main.rs
@@ -1,16 +1,17 @@
 fn main() {
     //println!("Length = {:?}", collatz_length(13).unwrap());
+    let mut lengths = [0; 1000001];
     let mut longest = 0;
-    for i in (1..=1000000).rev() {
-        let current = collatz_length(i).unwrap();
-        if current > longest{
-            longest = current;
+    for i in (1..=1000000) {
+        lengths[i] = collatz_length(i, &lengths).unwrap();
+        if lengths[i] > longest{
+            longest = lengths[i];
         }
     }
     println!("Longest = {:?}", longest);
 }
 
-fn collatz_step(n: u64) -> Option<u64>{
+fn collatz_step(n: usize) -> Option<usize>{
     if n == 1 { return None}
     match n%2 {
         0 => return Some(n/2),
@@ -19,11 +20,14 @@ fn collatz_step(n: u64) -> Option<u64>{
     }
 }
 
-fn collatz_length(n: u64) -> Result<u64, String>{
+fn collatz_length(n: usize, lens: &[usize]) -> Result<usize, String>{
     let mut length = 0;
     let mut inner = n;
     while let Some(step) = collatz_step(inner){
         length+=1;
+        if step<1000000 && lens[step] != 0 {
+            return Ok(lens[step]+length)
+        }
         inner=step;
         }
     Ok(length)
