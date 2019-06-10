54fdf022aab6dbbec56ec264b39f8024cad6fc23
Jon Moroney <darakian@gmail.com>
2019-06-10 10:17:31 -0700

    pe15

--- pe0015/src/main.rs
+++ pe0015/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", n_chose_k(40,20).unwrap());
+}
+
+fn n_chose_k(n: u128, k: u128) -> Option<u128> {
+    if k>n || k == 0 {return None}
+    Some((1..=k).fold( //https://en.wikipedia.org/wiki/Binomial_coefficient#Multiplicative_formula
+    1, |acc, i| 
+    acc*(n+1-i)/i
+    )) 
 }
