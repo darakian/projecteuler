798d322ec31b549de497a04b6d72c460656cb63d
Jon Moroney <darakian@gmail.com>
2019-08-09 15:43:01 -0700

    Working example

--- pe0024/src/main.rs
+++ pe0024/src/main.rs
@@ -1,24 +1,29 @@
-use std::collections::HashSet;
-
 fn main() {
-    println!("Hello, world! {:?}", lexi_permute("321".to_string()));
+    println!("{:?}", combinations(vec!['0','1','2']));
 }
 
-fn lexi_permute(s: String) -> Vec<String>{
+fn combinations(invec: Vec<char>) -> Vec<String> {
     let mut ret = Vec::new();
-    let mut chars: HashSet<_> = s.chars().collect();
-    for c in chars.iter() {
-        let single: HashSet<_> = vec![c.clone()].into_iter().collect::<std::collections::HashSet<_>>();
-        let append_strs = chars.difference(&single).collect();
-        println!("{:?} {:?}", c, combinations(append_strs));
+    for c in invec.into_iter(){
+        if ret.len() == 0{
+            ret.push(c.to_string());
+        } else {
+            let t_vec: Vec<String> = ret.drain(..).collect();
+            for s in t_vec.into_iter(){
+                ret.extend(all_strs(s, c).drain(..));
+            }
+        }
     }
+    ret.sort();
     ret
 }
 
-fn combinations(hesh: HashSet<&char>) -> Vec<&char> {
-    //let mut ret = Vec::new();
-    // for element in hesh.iter(){
-
-    // }
-    hesh.into_iter().collect()
+fn all_strs(invec: String, new_char: char) -> Vec<String>{
+    let mut ret = Vec::new();
+    for i in 0..=invec.len(){
+        let mut t_vec = invec.clone();
+        t_vec.insert(i, new_char);
+        ret.push(t_vec);
+    }
+    ret
 }
