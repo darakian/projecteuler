11a3413471baed41cd619b0bd46513aa12970fa7
Jon Moroney <darakian@gmail.com>
2019-10-04 16:53:28 -0700

    Close

--- pe0069/src/main.rs
+++ pe0069/src/main.rs
@@ -1,26 +1,41 @@
 use std::collections::HashSet;
 
 fn main() {
-    let test = 9;
-    println!("prime divs: {:?}", prime_divisors(test));
-    println!("totient: {:?}", totient(test));
+    let mut max_tot = 1.0;
+    let mut max_n = 1;
+    (1..=10)
+        .for_each(|x| {
+            let tmp = n_over_tot(x);
+            if tmp>max_tot{
+                    max_tot = tmp;
+                    max_n = x;
+                }
+            });
+    //println!("n:{}, n/tot:{}", max_n, max_tot);
+}
+
+fn n_over_tot(n: u64) -> f64{
+    let tot = totient(n);
+    println!("n:{} tot:{:?}", n, tot);
+    n as f64/tot as f64
 }
 
 fn totient(n: u64) -> u64{
+    if n == 1 {return 1}
     let mut prod = 1.0;
     let f_n = n as f64;
     for div in prime_divisors(n){
         if div == 1 {continue}
-        println!(">> (1.0 - 1.0/{})", div);
         prod = prod*(1.0-1.0/div as f64);
     }
+    //println!(">> n:{} tot:{:?}", n, (prod*f_n));
     (prod*f_n) as u64
 }
 
 fn prime_divisors(n: u64) -> HashSet<u64>{
     (1..=n)
-        .filter(|x| is_prime(*x))
         .filter(|x| n%x==0)
+        .filter(|x| is_prime(*x))
         .collect()
 }
 
