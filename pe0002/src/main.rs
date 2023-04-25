21e8fff3bf74329e5f78d1b93f5c51ef560b3b28
Jonathan Moroney <darakian@github.com>
2023-04-25 10:31:33 -0700

    Use an iterator

--- pe0002/src/main.rs
+++ pe0002/src/main.rs
@@ -1,20 +1,40 @@
 fn main() {
+    let fibber = Fibiter::new();
     let mut the_sum = 0;
-    let mut i = 1;
-    let mut j = 1;
-    while let current = fib_next(i, j){
+    for current in fibber.into_iter(){
         if current >= 4000000{
             break;
         }
         if current%2 == 0{
             the_sum+=current;
         }
-        i = j;
-        j = current;
     }
     println!("{}", the_sum);
 }
 
-fn fib_next(n: u32, m: u32) -> u32{
+fn fib_next(n: u64, m: u64) -> u64{
     return n+m
+}
+
+struct Fibiter {
+    first: u64,
+    second: u64,
+}
+
+impl Fibiter {
+    fn new() -> Fibiter {
+        Fibiter { first: 1, second: 1 }
+    }
+}
+
+
+impl Iterator for Fibiter {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let ret = fib_next(self.first, self.second);
+        self.first = self.second;
+        self.second = ret;
+        Some(ret)
+    }
 }
